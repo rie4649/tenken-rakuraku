575c5fc664eb13cc00390e662c57dd6f45c46589
rie4649 <okm.co@icloud.com>
2026-07-05 18:56:23 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,61 @@
-const CACHE_NAME = "tenken-rakuraku-v6";
+const CACHE_NAME = "tenken-rakuraku-v7";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./setting.html",
-  "./qr.html",
-  "./qr-print.html",
-  "./print-day.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png"
 ];
 
 self.addEventListener("install", function(event){
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache){
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.skipWaiting();
+event.waitUntil(
+caches.open(CACHE_NAME).then(function(cache){
+return cache.addAll(FILES_TO_CACHE);
+})
+);
 });
 
 self.addEventListener("activate", function(event){
-  event.waitUntil(
-    caches.keys().then(function(keys){
-      return Promise.all(
-        keys.map(function(key){
-          if(key !== CACHE_NAME){
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+event.waitUntil(
+caches.keys().then(function(keys){
+return Promise.all(
+keys.map(function(key){
+if(key !== CACHE_NAME){
+return caches.delete(key);
+}
+})
+);
+}).then(function(){
+return self.clients.claim();
+})
+);
 });
 
 self.addEventListener("fetch", function(event){
-  event.respondWith(
-    caches.match(event.request).then(function(response){
-      return response || fetch(event.request);
-    })
-  );
+if(event.request.method !== "GET")return;
+
+const url = new URL(event.request.url);
+const isAppFile = url.origin === location.origin &&
+(event.request.destination === "document" || url.pathname.endsWith(".js") || url.pathname.endsWith(".css"));
+
+if(isAppFile){
+event.respondWith(
+fetch(event.request).then(function(response){
+const copy = response.clone();
+caches.open(CACHE_NAME).then(function(cache){
+cache.put(event.request, copy);
+});
+return response;
+}).catch(function(){
+return caches.match(event.request);
+})
+);
+return;
+}
+
+event.respondWith(
+caches.match(event.request).then(function(response){
+return response || fetch(event.request);
+})
+);
 });
